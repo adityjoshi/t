7098a1bd3a5044df3c36bd1bde718674fc93cdb1
Aditya Joshi <aditya03joshi@gmail.com>
2024-06-11 20:09:04 +0530

    java

---
+++ entities/torrent.go
@@ -0,0 +1,16 @@
+package entities 
+
+
+type FileInfo struct {
+  Length int64
+  PieceLength int64
+  Name string
+  Pieces []SHAhash
+}
+
+type Torrent struct {
+  Announce string 
+  Info FileInfo 
+  InfoRaw  map[string]interface{}
+}
+
