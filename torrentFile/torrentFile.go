5ccf59b11e4d60a8c8cad44a22767125b8bc99bf
Aditya Joshi <aditya03joshi@gmail.com>
2024-06-14 17:49:30 +0530

    torrentFile batch

--- torrentFile/torrentFile.go
+++ torrentFile/torrentFile.go
@@ -13,6 +13,16 @@ import (
 
 func batch(data []byte, batch int) []entities.SHAhash {
 	var result []entities.SHAhash
+  for i:= 0; i<len(data); i+= batch {
+    hash := entities.SHAhash{}
+    end := 1 + batch
+    if end > len(data) {
+      end = len(data)
+    }
+    copy(hash[:], data[i:end])
+		result = append(result, hash)
+  }
+  return result 
 }
 
 func getTorrent(bReader *bufio.Reader) (*entities.Torrent, error) {
@@ -32,13 +42,13 @@ func getTorrent(bReader *bufio.Reader) (*entities.Torrent, error) {
 	torrent := &entities.Torrent{}
 	torrent.Announce = torrentData["announce"].(string)
 	torrent.Info = entities.FileInfo{
-    PieceLength: torrentInfoData["PieceLength"].(int64),
-    Length: torrentInfoData["length"].(int64),
-    Name: torrentInfoData["name"].(string),
-    Pieces: batch([]byte(torrentInfoData["pieces"].(string)),20),
-  }
-  torrent.InfoRaw = torrentData["info"].(map[string]interface{})
-  return torrent, nil
+		PieceLength: torrentInfoData["PieceLength"].(int64),
+		Length:      torrentInfoData["length"].(int64),
+		Name:        torrentInfoData["name"].(string),
+		Pieces:      batch([]byte(torrentInfoData["pieces"].(string)), 20),
+	}
+	torrent.InfoRaw = torrentData["info"].(map[string]interface{})
+	return torrent, nil
 }
 
 func ParseTorrentFile(filePath string) (*entities.Torrent, error) {
