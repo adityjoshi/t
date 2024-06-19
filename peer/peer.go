ce3f622456d78cb1112b79740a5d5e67c851c321
Aditya Joshi <aditya03joshi@gmail.com>
2024-06-19 18:58:10 +0530

    peers Unmarshal

--- peer/peer.go
+++ peer/peer.go
@@ -1,10 +1,25 @@
 package peer
 
-import "github.com/adityjoshi/t/entities"
+import (
+	"encoding/binary"
+	"errors"
+	"net"
 
+	"github.com/adityjoshi/t/entities"
+)
 
-func Unmarshal(peersBin []byte) (entities.Peer, error) {
-  // Peers or peersSize is another long binary blob which holds the ip address and port the first 4 bit stores the ip and the last 2 store port
-  peerSize := 6 
-
+func Unmarshal(peersBin []byte) ([]entities.Peer, error) {
+	// Peers or peersSize is another long binary blob which holds the ip address and port the first 4 bit stores the ip and the last 2 store port
+	peerSize := 6
+	numPeers := len(peersBin) / peerSize
+	if len(peersBin)%peerSize != 0 {
+		return nil, errors.New("Invalid Error")
+	}
+	peers := make([]entities.Peer, numPeers)
+	for i := 0; i < numPeers; i++ {
+		offset := i * peerSize
+		peers[i].IP = net.IP(peersBin[offset : offset+4]).String()
+		peers[i].Port = int64(binary.BigEndian.Uint16([]byte(peersBin[offset+4 : offset+6])))
+	}
+	return peers, nil
 }
