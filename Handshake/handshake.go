19addfa0b606261f0a3f72329d13e1dd301db4f0
Aditya Joshi <aditya03joshi@gmail.com>
2024-06-23 13:21:48 +0530

    handshake serialize & read

--- Handshake/handshake.go
+++ Handshake/handshake.go
@@ -1,5 +1,10 @@
 package handshake
 
+import (
+	"fmt"
+	"io"
+)
+
 type Handshake struct {
 	Pstr     string
 	InfoHash [20]byte
@@ -17,15 +22,48 @@ func New(infoHash, peerID [20]byte) *Handshake {
 /*
 When we serialize this is how a handshake string looks like
 \x13BitTorrent protocol\x00\x00\x00\x00\x00\x00\x00\x00\x86\xd4\xc8\x00\x24\xa4\x69\xbe\x4c\x50\xbc\x5a\x10\x2c\xf7\x17\x80\x31\x00\x74-TR2940-k8hj0wgej6ch
+this method is used to send the our handshake
 */
 func (h *Handshake) Serialize() []byte {
-  buf := make([]byte, len(h.Pstr)+49)
-  buf[0] = byte(len(h.Pstr))
-  curr := 1
-  curr += copy(buf[curr:],h.Pstr)
-  curr += copy(buf[curr:],make([]byte,8)) // 8 reserved bytes
-  curr += copy(buf[curr:],h.InfoHash[:])
-  curr += copy(buf[curr:],h.PeerID[:])
-  return buf
+	buf := make([]byte, len(h.Pstr)+49)
+	buf[0] = byte(len(h.Pstr))
+	curr := 1
+	curr += copy(buf[curr:], h.Pstr)
+	curr += copy(buf[curr:], make([]byte, 8)) // 8 reserved bytes
+	curr += copy(buf[curr:], h.InfoHash[:])
+	curr += copy(buf[curr:], h.PeerID[:])
+	return buf
+}
 
+/*
+Read func parses the handshake from the stream
+it is used to get the handshake info from the peer and parse it
+*/
+func Read(r io.Reader) (*Handshake, error) {
+	lengthBuf := make([]byte, 1)
+	_, err := io.ReadFull(r, lengthBuf)
+	if err != nil {
+		return nil, err
+	}
+	pstrlen := int(lengthBuf[0])
+
+	if pstrlen == 0 {
+		err := fmt.Errorf("pstrlen cannot be 0")
+		return nil, err
+	}
+	handshakeBuf := make([]byte, 48+pstrlen)
+	_, err = io.ReadFull(r, handshakeBuf)
+	if err != nil {
+		return nil, err
+	}
+	var infoHash, peerID [20]byte
+	copy(infoHash[:], handshakeBuf[pstrlen+8:pstrlen+8+20])
+	copy(peerID[:], handshakeBuf[pstrlen+8+20:])
+
+	h := Handshake{
+		Pstr:     string(handshakeBuf[0:pstrlen]),
+		InfoHash: infoHash,
+		PeerID:   peerID,
+	}
+	return &h, nil
 }
