2b928836cff5dc1a0f12e92fd3ba88bec736ea83
Aditya Joshi <aditya03joshi@gmail.com>
2024-06-23 15:17:49 +0530

    message

---
+++ message/message.go
@@ -0,0 +1,24 @@
+package message
+
+type messageID uint8
+
+const (
+	// MessageChoke chokes the receiver
+	MessageChoke messageID = 0
+	// MessageUnchoke unchokes the receiver
+	MessageUnchoke messageID = 1
+	// MessageInterested interested in receiving data
+	MessageInterested messageID = 2
+	// MessageUninterested not interesred in data
+	MessageUninterested messageID = 3
+	// MessageHave alerts the receiver that sender has downloaded the piece
+	MessageHave messageID = 4
+	// MessageBitfield encodes which piece sender has downloaded
+	MessageBitfield messageID = 5
+	// MessageRequest request a block of data from receiver
+	MessageRequest messageID = 6
+	// MessagePiece delivers a block of data to fulfill request
+	MessagePiece messageID = 7
+	// MessageCancel cancles the request
+	MessageCancel messageID = 8
+)
