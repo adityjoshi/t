07d46e9bf8bcc3de01589d85d1ea8b316e87a21b
Aditya Joshi <aditya03joshi@gmail.com>
2024-06-24 12:20:00 +0530

    message request

--- message/message.go
+++ message/message.go
@@ -34,6 +34,20 @@ type Message struct {
 	Payload []byte
 }
 
+func FormatRequest(index, begin, length int) *Message {
+	payload := make([]byte, 12)
+	binary.BigEndian.PutUint32(payload[0:4], uint32(index))
+	binary.BigEndian.PutUint32(payload[4:8], uint32(begin))
+	binary.BigEndian.PutUint32(payload[8:12], uint32(length))
+	return &Message{ID: MessageRequest, Payload: payload}
+}
+
+func FormatHave(index int) *Message {
+	payload := make([]byte, 4)
+	binary.BigEndian.PutUint32(payload, uint32(index))
+	return &Message{ID: MessageHave, Payload: payload}
+}
+
 /*
 a message has three fields <length><id><optional Payload>
 length is of size 4 , id is of size 1 and the remaining is paylod
