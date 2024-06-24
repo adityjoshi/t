96f55cfe52b27e15ce1f76aef69e61c1b434d653
Aditya Joshi <aditya03joshi@gmail.com>
2024-06-24 13:02:07 +0530

    message

--- message/message.go
+++ message/message.go
@@ -34,6 +34,7 @@ type Message struct {
 	Payload []byte
 }
 
+// FormatRequest creates a Request message
 func FormatRequest(index, begin, length int) *Message {
 	payload := make([]byte, 12)
 	binary.BigEndian.PutUint32(payload[0:4], uint32(index))
@@ -42,10 +43,50 @@ func FormatRequest(index, begin, length int) *Message {
 	return &Message{ID: MessageRequest, Payload: payload}
 }
 
+// FormatHave creates a Have message
 func FormatHave(index int) *Message {
 	payload := make([]byte, 4)
 	binary.BigEndian.PutUint32(payload, uint32(index))
 	return &Message{ID: MessageHave, Payload: payload}
+}
+
+func ParsePiece(index int, buf []byte, msg *Message) (int, error) {
+	if msg.ID != MessagePiece {
+		return 0, fmt.Errorf("Expected PIECE (ID %d), got ID %d", MessagePiece, msg.ID)
+	}
+	if len(msg.Payload) < 8 {
+		return 0, fmt.Errorf("Payload too short. %d < 8", len(msg.Payload))
+	}
+	parsedIndex := int(binary.BigEndian.Uint32(msg.Payload[0:4]))
+	if parsedIndex != index {
+		return 0, fmt.Errorf("Expected index %d, got %d", index, parsedIndex)
+	}
+	begin := int(binary.BigEndian.Uint32(msg.Payload[4:8]))
+	if begin >= len(buf) {
+		return 0, fmt.Errorf("Begin offset too high. %d >= %d", begin, len(buf))
+	}
+	data := msg.Payload[8:]
+	if begin+len(data) > len(buf) {
+		return 0, fmt.Errorf(
+			"Data too long [%d] for offset %d with length %d",
+			len(data),
+			begin,
+			len(buf),
+		)
+	}
+	copy(buf[begin:], data)
+	return len(data), nil
+}
+
+func ParseHave(msg *Message) (int, error) {
+	if msg.ID != MessageHave {
+		return 0, fmt.Errorf("Expected HAVE (ID %d), got ID %d", MessageHave, msg.ID)
+	}
+	if len(msg.Payload) != 4 {
+		return 0, fmt.Errorf("Expected payload length 4, got length %d", len(msg.Payload))
+	}
+	index := int(binary.BigEndian.Uint32(msg.Payload))
+	return index, nil
 }
 
 /*
@@ -114,4 +155,11 @@ func (m *Message) name() string {
 	default:
 		return fmt.Sprintf("Unknown#%d", m.ID)
 	}
+}
+
+func (m *Message) String() string {
+	if m == nil {
+		return m.name()
+	}
+	return fmt.Sprintf("%s [%d]", m.name(), len(m.Payload))
 }
