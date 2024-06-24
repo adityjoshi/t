e17be3aaf8b3254732039b87555b4439bb507003
Aditya Joshi <aditya03joshi@gmail.com>
2024-06-24 10:19:33 +0530

    message serialize

--- message/message.go
+++ message/message.go
@@ -1,5 +1,7 @@
 package message
 
+import "encoding/binary"
+
 type messageID uint8
 
 const (
@@ -24,8 +26,21 @@ const (
 )
 
 type Message struct {
-  ID messageID
-  Payload []byte
+	ID      messageID
+	Payload []byte
 }
-
-
+/*
+a message has three fields <length><id><optional Payload>
+length is of size 4 , id is of size 1 and the remaining is paylod 
+*/
+func (m *Message) Serialize() []byte {
+	if m == nil {
+		return make([]byte, 4)
+	}
+	length := uint32(len(m.Payload) + 1)
+	buf := make([]byte, 4+length)
+	binary.BigEndian.PutUint32(buf[0:4], length)
+	buf[4] = byte(m.ID)
+	copy(buf[5:], m.Payload)
+	return buf
+}
