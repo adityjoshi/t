a15e10aa6c68754212cb43b1474b5ef3d19559cd
Aditya Joshi <aditya03joshi@gmail.com>
2024-06-28 13:20:40 +0530

    client completed

--- client/client.go
+++ client/client.go
@@ -1,27 +1,86 @@
 package client
 
 import (
+	"bytes"
+	"fmt"
 	"net"
 	"time"
-  "github.com/adityjoshi/t/peer"
+
+	handshake "github.com/adityjoshi/t/Handshake"
+	"github.com/adityjoshi/t/bitfield"
 	"github.com/adityjoshi/t/entities"
+	"github.com/adityjoshi/t/message"
+	"github.com/adityjoshi/t/peer"
 )
 
-
-
 type Client struct {
-	Conn   net.Conn
-	Choked bool
-	// add bitfield
+	Conn     net.Conn
+	Choked   bool
+	Bitfield bitfield.Bitfield
 	Peer     entities.Peer
-	infoHash [20]byte 
+	infoHash [20]byte
 	peerID   [20]byte
 }
 
+func completeHandshake(conn net.Conn, infohash, peerID [20]byte) (*handshake.Handshake, error) {
+	conn.SetDeadline(time.Now().Add(3 * time.Second))
+	defer conn.SetDeadline(time.Time{}) // Disable the deadline
 
-func NewConn(peer peer.WrapperPeer,peerID,infoHash[20]byte) (*Client, error) {
-  conn , err := net.DialTimeout("tcp",peer.String(),3*time.Second) 
-  if err != nil {
-    return nil, err
-  }
+	req := handshake.New(infohash, peerID)
+	_, err := conn.Write(req.Serialize())
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := handshake.Read(conn)
+	if err != nil {
+		return nil, err
+	}
+	if !bytes.Equal(res.InfoHash[:], infohash[:]) {
+		return nil, fmt.Errorf("Expected infohash %x but got %x", res.InfoHash, infohash)
+	}
+	return res, nil
+}
+
+func recieveBitfield(conn net.Conn) (bitfield.Bitfield, error) {
+	conn.SetDeadline(time.Now().Add(5 * time.Second))
+	defer conn.SetDeadline(time.Time{}) // Disable the deadline
+
+	msg, err := message.Read(conn)
+	if err != nil {
+		return nil, err
+	}
+	if msg.ID != message.MessageBitfield {
+		err := fmt.Errorf("Expected bitfield but got ID %d", msg.ID)
+		return nil, err
+	}
+
+	return msg.Payload, nil
+}
+
+func New(peer peer.WrapperPeer, peerID, infoHash [20]byte) (*Client, error) {
+	conn, err := net.DialTimeout("tcp", peer.String(), 3*time.Second)
+	if err != nil {
+		return nil, err
+	}
+  _, err = completeHandshake(conn, infoHash, peerID)
+	if err != nil {
+		conn.Close()
+		return nil, err
+	}
+
+	bf, err := recieveBitfield(conn)
+	if err != nil {
+		conn.Close()
+		return nil, err
+	}
+
+	return &Client{
+		Conn:     conn,
+		Choked:   true,
+		Bitfield: bf,
+		Peer:     entities.Peer{}, 
+		infoHash: infoHash,
+		peerID:   peerID,
+	}, nil
 }
