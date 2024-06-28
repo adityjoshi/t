cbe1594ceb05b432bd97d1e889348f0bd5908f3c
Aditya Joshi <aditya03joshi@gmail.com>
2024-06-28 12:49:19 +0530

    bitfield

--- bitfield/bitfield.go
+++ bitfield/bitfield.go
@@ -2,12 +2,26 @@ package bitfield
 
 type Bitfield []byte
 
+/*
+byteIndex tells the byte position
+offset tells the position within the byte
+then the boundarycheck
+*/
 
-func (bf Bitfield ) HasPiece(index int) bool {
-  byteIndex := index/8
-  offset := index%8
-  if byteIndex < 0 || byteIndex >= len(bf) {
-    return false
-  }
-  return bf[byteIndex]>>(7-offset)&1 != 0
+func (bf Bitfield) HasPiece(index int) bool {
+	byteIndex := index / 8
+	offset := index % 8
+	if byteIndex < 0 || byteIndex >= len(bf) {
+		return false
+	}
+	return bf[byteIndex]>>(7-offset)&1 != 0
+}
+
+func (bf Bitfield) SetPiece(index int) {
+	byteIndex := index / 8
+	offset := index % 8
+	if byteIndex < 0 || byteIndex >= len(bf) {
+		return
+	}
+	bf[byteIndex] |= 1 << (7 - offset)
 }
