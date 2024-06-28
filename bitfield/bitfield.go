b562ee238697a029f89d7d444878a67a28fda8c4
Aditya Joshi <aditya03joshi@gmail.com>
2024-06-28 11:20:58 +0530

    bitfield

---
+++ bitfield/bitfield.go
@@ -0,0 +1,13 @@
+package bitfield
+
+type Bitfield []byte
+
+
+func (bf Bitfield ) HasPiece(index int) bool {
+  byteIndex := index/8
+  offset := index%8
+  if byteIndex < 0 || byteIndex >= len(bf) {
+    return false
+  }
+  return bf[byteIndex]>>(7-offset)&1 != 0
+}
